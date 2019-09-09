490ba11be19a940d749e70c9ebdb9e53232384af
Dragoș Perju <dragosprju@gmail.com>
2019-09-09 10:27:33 +0200

    Python script now holds plot, exposes plot pin colors

--- Python/Experiments/experiment.py
+++ Python/Experiments/experiment.py
@@ -1,5 +1,23 @@
-from pydgilib_extra import DGILibExtra, LOGGER_PLOT, LOGGER_CSV
+from pydgilib_extra import DGILibExtra, LOGGER_PLOT, LOGGER_CSV, LOGGER_OBJECT
+import os
+from matplotlib import pyplot as plt
 
-with DGILibExtra(loggers=[LOGGER_PLOT, LOGGER_CSV]) as dgilib:
+def wait_for_plot(fig):
+    if platform == "linux" or platform == "linux2":
+        while plt.fignum_exists(fig.number):
+            plt.pause(0.000001)
+
+dgilib_config = {
+    "loggers" : [
+        LOGGER_OBJECT,
+        LOGGER_CSV,
+        LOGGER_PLOT
+    ],
+    "plot_pins_colors": ["red", "yellow", "blue", "green"]
+}
+
+with DGILibExtra(**dgilib_config) as dgilib:
     dgilib.device_reset()
-    dgilib.logger.log(10)
+    data = dgilib.logger.log(10) # Afterwards, can process data in RAM
+
+    plt.show(block=True) # Block figure from closing at the end of the script
